4b206ecbad7cbfc60c20f3b75f8d3fd8abf819d4
atif2618 <151423780+atif2618@users.noreply.github.com>
2025-09-21 19:10:55 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:2000', // 👈 point to your backend
+  baseURL: 'truegradient-assigment-gq6u.vercel.app', // 👈 point to your backend
   withCredentials: true // if you use cookies
 });
 
 export default api;
+
